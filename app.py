f4f307da867965c9446f8a5f5e2c82ec661f3bad
Hricha Jaiswal <99092506+Hricha-1511@users.noreply.github.com>
2024-05-07 22:59:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ async def ask_question(question: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=9000)
+    uvicorn.run(app, host="127.0.0.1", port=$PORT)
